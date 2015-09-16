b926857a80dcb0e53bf66cc4694bbb6061bdf111
Ryan Betker <technologyRyan@outlook.com>
2015-09-16 16:27:57 -0500

    PerfHub code and classes

---
+++ PerfSurf/Counters/PerfCounterService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerfSurf.Counters
+{
+    public class PerfCounterService
+    {
+        private List<PerfCounterWrapper> _counters;
+        public PerfCounterService()
+        {
+            _counters = new List<PerfCounterWrapper>();
+
+            _counters.Add(new PerfCounterWrapper(name: "Processor", category: "Processor", counter: "", instance: ""));
+
+            _counters.Add(new PerfCounterWrapper("Paging", "Memory", "Pages/sec"));
+
+            _counters.Add(new PerfCounterWrapper("Disk", "Physical Disk", "% Disk Time", "_Total"));
+                        
+        }
+
+        public dynamic GetResults()
+        {
+            return _counters.Select(c => new 
+                    { name = c.Name, value = c.Value }
+                );
+        }
+    }
+}
---
+++ PerfSurf/Counters/PerfCounterWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace PerfSurf.Counters
+{
+    public class PerfCounterWrapper
+    {
+        public string Name { get; set; }
+        public float Value { get { return _counter.NextValue(); } }
+
+        private PerformanceCounter _counter;
+
+        public PerfCounterWrapper(string name, string category, string counter, string instance = "")
+        {
+            _counter = new PerformanceCounter(category, name, instance, readOnly: true);
+
+            this.Name = name;
+        }
+    }
+}
