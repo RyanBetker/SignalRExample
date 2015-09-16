4e974fe2e9dd0549f1ba0cbeafdc773e3ef0866e
Ryan Betker <technologyRyan@outlook.com>
2015-09-16 18:02:04 -0500

    Has issue to fix with Owin.dll trying to target 3.0 because of a web.config redirect, but only 2.1 exists on disk.

---
+++ PerfSurf/Scripts/perfSurf.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    var perfHub = $.connection.perfHub;
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+    perfHub.client.newMessage = function (message) {
+        //display to UI using Knockout
+        model.addMessage(message);
+    };
+
+    perfHub.client.newCounters = function (counters) {
+        model.addCounters(counters);
+    };
+
+    var ChartEntry = function (name) {
+        var self = this;
+
+        self.name = name;
+    };
+    ChartEntry.prototype
+
+    var Model = function () {
+        var self = this;
+        self.message = ko.observable("");
+        self.messages = ko.observableArray();
+        self.counters = ko.observableArray();
+    };
+
+    Model.prototype = {
+        addCounters: function(updatedCounters) {
+            var self = this;
+
+            $.each(updatedCounters, function (index, updateCounter)
+            {
+                //find an existing entry if present
+                var entry = ko.utils.arrayFirst(self.counters(),
+                    function (counter)
+                    {
+                        return counter.name == updateCounter.name;
+                    });
+                if (!entry) {
+                    entry = new ChartEntry(updat.name);
+                    self.counters.push(entry);
+                }
+            });
+        },
+        sendMessage: function () {
+            var self = this;
+            perfHub.server.send(self.message());
+            self.message("");
+        },
+        addMessage: function (message) {
+            var self = this;
+            self.messages.push(message);
+        }
+    };
+
+})
